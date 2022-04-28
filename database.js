d0e8123d48d2bfee89d7027fa7c52214379000b5
Pramit Thapa <69735326+pramit41@users.noreply.github.com>
2022-04-28 14:55:48 -0400

    finished with database

--- database.js
+++ database.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const database = require('better-sqlite3')
 
 const logdb = new database('log.db') 
@@ -8,6 +10,23 @@ let row = stmt.get()
 
 if(row == undefined){
     console.log('Your database appears to be empty. I will initialize it now.')
+
+    const sqlInit = `CREATE TABLE accesslog (
+        id INTEGER PRIMARY KEY,
+        remoteaddr VARCHAR,
+        remoteuser VARCHAR,
+        time VARCHAR,
+        method VARCHAR,
+        url VARCHAR,
+        protocol VARCHAR, 
+        httpversion NUMERIC,
+        status INTEGER,
+        referer VARCHAR,
+        useragent VARCHAR
+    )`
+
+    logdb.exec(sqlInit)
+    console.log('Your database has now been initialize')
 } else {
     console.log('Database exits')
 }
