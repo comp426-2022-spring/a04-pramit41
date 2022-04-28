927b849bb9d78bdcf2b4223e985bc5a58702a457
Pramit Thapa <pramit41@email.unc.edu>
2022-04-28 16:13:46 -0400

    fixed db

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const database = require('better-sqlite3')
 
 const logdb = new database('log.db') 
 
-const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
+const stmt = logdb.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
 
 let row = stmt.get()
 
